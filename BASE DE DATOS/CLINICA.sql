4dcccb8223d5944743f2f9de8f224e4bd70d447f
HabanoVarado <94261027+HabanoVarado@users.noreply.github.com>
2022-12-01 00:06:54 -0500

    Add files via upload

---
+++ BASE DE DATOS/CLINICA.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE CLINICA
+
+USE CLINICA
+GO 
+
+ CREATE TABLE UNIDADES(
+ idUnidad int primary key,
+ NomPlanta varchar(30) NOT NULL,
+ NroPlanta TINYINT NOT NULL,
+ )
+
+ CREATE TABLE Director(
+ idDirector INT PRIMARY KEY IDENTITY (1,1),
+ NombreDirector VARCHAR(50) NOT NULL,
+ Especialidad VARCHAR (30) NOT NULL,
+ CONSTRAINT fk_UNIDADES FOREIGN KEY (idUnidades)  REFERENCES UNIDADES
+ )
+
+ CREATE TABLE UNIDADPACIENTES(
+ UnidadPaciente INT PRIMARY KEY IDENTITY (1,1),
+ CONSTRAINT fk_UNIDADES FOREIGN KEY (idUnidad) REFERENCES UNIDADES (idUnidad)
+ CONSTRAINT fk_Paciente FOREIGN KEY (idPaciente) REFERENCES Pacientes ( idPaciente)
+ FECHA TINYINT 
+ )
+
+ CREATE TABLE Paciente (
+ idPaciente INT PRIMARY KEY ,
+ NombrePaciente VARCHAR (50),
+ Edad TINYINT
+ )
+
+CREATE TABLE Ingreso (
+idIngreso INT PRIMARY KEY IDENTITY (1,1),
+CONSTRAINT fk_idPaciente FOREIGN KEY (idPaciente) REFERENCES Pacientes ( idPacientes),
+FechaIngreso TINYINT,
+FechaSalida TINYINT,
+)
